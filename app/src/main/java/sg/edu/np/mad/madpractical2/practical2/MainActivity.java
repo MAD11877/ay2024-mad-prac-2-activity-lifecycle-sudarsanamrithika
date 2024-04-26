21e64cc2171e8a493f725440e2825587d500693a
sudarsanamrithika <veethe2nd@gmail.com>
2024-04-26 14:23:54 +0800

    attempt 4

--- app/src/main/java/sg/edu/np/mad/madpractical2/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/practical2/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Initialize a new User object
-        User user = new User("John Doe","@strings/text", 1, false);
+        User user = new User("John Doe","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 1, false);
 
         // Get the TextViews and Button from the layout
         TextView tvName = findViewById(R.id.textView);

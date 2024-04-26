5cf340e80678dbe16076600096f09e18b9ed51a8
sudarsanamrithika <veethe2nd@gmail.com>
2024-04-26 13:54:16 +0800

    attempt 1

--- app/src/main/java/sg/edu/np/mad/madpractical2/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/practical2/MainActivity.java
@@ -8,7 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize a new User object
+        User user = new User("John Doe","MAD Developer", 1, false);
+
+        // Get the TextViews and Button from the layout
+        TextView tvName = findViewById(R.id.textView);
+        TextView tvDescription = findViewById(R.id.textView2);
+        Button btnFollow = findViewById(R.id.button);
+
+//        // Get the TextViews with the User's name, description and default button message
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("Follow");
+
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (user.followed) {
+                    btnFollow.setText("Follow");
+                    user.followed = false;
+                } else {
+                    btnFollow.setText("Unfollow");
+                    user.followed = true;
+                }
+            }
+
+
+        });
+
     }
+
 }

f369f9ff12da36cc4c4c726407687e08f76c21e2
sudarsanamrithika <veethe2nd@gmail.com>
2024-04-29 18:26:28 +0800

    delete that one part

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -1,16 +1,20 @@
-package sg.edu.np.mad.madpractical2.practical2;
+package sg.edu.np.mad.madpractical2;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
 import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +27,35 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        User user = new User("John Doe", "MAD Developer", 1, false);
+        // Initialize a new User object
+        User user = new User("John Doe","MAD Developer", 1, false);
+
+        // Get the TextViews and Button from the layout
         TextView tvName = findViewById(R.id.tvName);
         TextView tvDescription = findViewById(R.id.tvDescription);
         Button btnFollow = findViewById(R.id.btnFollow);
 
+//        // Get the TextViews with the User's name, description and default button message
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("FOLLOW");
+
         btnFollow.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 if (user.followed) {
-                    btnFollow.setText("Follow");
+                    btnFollow.setText("FOLLOW");
                     user.followed = false;
-                }
-                else{
-                    btnFollow.setText("Unfollow");
+                } else {
+                    btnFollow.setText("UNFOLLOW");
                     user.followed = true;
                 }
             }
+
+
         });
+
     }
+
 }
+
--- app/src/main/java/sg/edu/np/mad/madpractical2/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/User.java
@@ -1,4 +1,4 @@
-package sg.edu.np.mad.madpractical2.practical2;
+package sg.edu.np.mad.madpractical2;
 
 public class User {
     public String name;
@@ -11,5 +11,6 @@ public class User {
         this.id = id;
         this.followed = followed;
     }
+
 }
 

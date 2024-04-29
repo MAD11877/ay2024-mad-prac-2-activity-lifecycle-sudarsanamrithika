d41d4435af939bbb0f71f4a712db6e67d7a68758
sudarsanamrithika <veethe2nd@gmail.com>
2024-04-29 17:08:04 +0800

    another attempt at fixing

--- app/src/main/java/sg/edu/np/mad/madpractical2/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/practical2/MainActivity.java
@@ -1,5 +1,6 @@
 package sg.edu.np.mad.madpractical2.practical2;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -27,8 +28,18 @@ public class MainActivity extends AppCompatActivity {
         TextView tvDescription = findViewById(R.id.tvDescription);
         Button btnFollow = findViewById(R.id.btnFollow);
 
-        tvName.setText(user.name);
-        tvDescription.setText(user.description);
-        btnFollow.setText("Follow");
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.followed) {
+                    btnFollow.setText("Follow");
+                    user.followed = false;
+                }
+                else{
+                    btnFollow.setText("Unfollow");
+                    user.followed = true;
+                }
+            }
+        });
     }
 }
